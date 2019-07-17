3f049a5f84a26578923bef9089430f2c5e6ba84d
Carl Barringer <cbarringer@Carls-MacBook-Pro.local>
2019-07-17 14:47:19 -0500

    Initial Commit

---
+++ main.js
@@ -0,0 +1,24 @@
+const sangar = {
+    Name: ["Elizabeth", "Sangar"],
+    congressionalDistrict: "Tennessee District 5",
+    platformStatements: ["Taxes", "Jobs", "Infrastructure", "Health care", "Crime and enforcement"],
+    donationURL: "https://www.ElizabethSangar.com/donations",
+    calendar: "",
+    biography: "",
+    missionStatement: "",
+    registerVoteURL: "https://ovr.govote.tn.gov/",
+}
+const volunteers = {
+    name: "",
+    address: "",
+    email: "",
+    phoneNumber: "",
+    availability: "",
+    activities: [],
+}
+const imageGallery = {
+    headShot: "",
+    familyPic: "",
+    ConstituentPic: "",
+},
+
